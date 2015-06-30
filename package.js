09c8b2c9c0da57b62f307de7905a0d6cda2fffd8
Merlin Patterson <merlinpatt@gmail.com>
2015-06-29 21:42:09 -0400

    Add versions for publishing

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'accounts-edmodo',
+  name: 'merlin:accounts-edmodo',
   summary: 'Login service for Edmodo accounts',
   git: 'https://github.com/merlinpatt/accounts-edmodo',
   version: '0.9.0',
@@ -7,11 +7,11 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.use('accounts-base', ['client', 'server']);
+  api.use('accounts-base@1.2.0', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('edmodo', ['client', 'server']);
+  api.use('accounts-oauth@1.1.5', ['client', 'server']);
+  api.use('merlin:edmodo@0.9.0', ['client', 'server']);
 
   api.addFiles('edmodo_login_button.css', 'client');
 
