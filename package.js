0dab659eec346e68413b5cf27e3ac1845886e4b6
Merlin Patterson <merlinpatt@gmail.com>
2015-08-06 15:37:47 -0400

    Update version, include Edmodo CSS, use Less

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'merlin:accounts-edmodo',
   summary: 'Login service for Edmodo accounts',
   git: 'https://github.com/merlinpatt/accounts-edmodo',
-  version: '1.0.0',
+  version: '1.0.1',
   documentation: 'README.md'
 });
 
@@ -11,9 +11,10 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth@1.1.5', ['client', 'server']);
-  api.use('merlin:edmodo@1.0.0', ['client', 'server']);
+  api.use('less@1.0.14', ['client', 'server']);
+  api.use('merlin:edmodo@1.0.1', ['client', 'server']);
 
-  api.addFiles('edmodo_login_button.css', 'client');
+  api.addFiles('edmodo_login_button.less', 'client');
 
   api.addFiles('edmodo.js');
 });
